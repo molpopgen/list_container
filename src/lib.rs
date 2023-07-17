5ddbec220b59fa6b79936bf6c37aec0ca073b603
Kevin R. Thornton <krthornt@uci.edu>
2023-07-17 09:22:44 -0700

    internal fixes and insert_before

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,26 @@ impl<T> ListContainer<T> {
         }
         *cursor = internal_cursor;
         self.cursor[cursor.current.0] = internal_cursor;
+        self.cursor[new_cursor.current.0] = new_cursor;
+        new_cursor
+    }
+
+    pub fn insert_before(&mut self, cursor: &mut Cursor, datum: T) -> Cursor
+    {
+        let mut new_cursor = self.new_cursor(datum);
+        let mut internal_cursor = self.cursor[cursor.current.0];
+        if let Some(prev) = internal_cursor.prev {
+            internal_cursor.prev = Some(new_cursor.current);
+            new_cursor.next = Some(internal_cursor.current);
+            new_cursor.prev = Some(prev);
+            self.cursor[prev.0].next = Some(new_cursor.current);
+        } else {
+            internal_cursor.prev = Some(new_cursor.current);
+            new_cursor.next = Some(internal_cursor.current);
+        }
+        *cursor = internal_cursor;
+        self.cursor[cursor.current.0] = internal_cursor;
+        self.cursor[new_cursor.current.0] = new_cursor;
         new_cursor
     }
 
@@ -96,11 +116,15 @@ fn test_insert_after() {
     let mut list = ListContainer::<i32>::default();
     let mut head = list.add_list(0);
     let mut next0 = list.insert_after(&mut head, 1);
-    let next1 = list.insert_after(&mut next0, 2);
+    let mut next1 = list.insert_after(&mut next0, 2);
 
     assert_eq!(list.data[head.current.0], 0);
     assert_eq!(list.data[next0.current.0], 1);
     assert_eq!(list.data[next1.current.0], 2);
+
+    for i in &list.cursor {
+        println!("before {i:?}");
+    }
 
     let forward = list.iterate_forward(head).cloned().collect::<Vec<_>>();
     assert_eq!(
@@ -112,5 +136,19 @@ fn test_insert_after() {
         next0,
         next1
     );
-}
 
+    let _ = list.insert_before(&mut next1, 7);
+    for i in &list.cursor {
+        println!("after {i:?}");
+    }
+    let forward = list.iterate_forward(head).cloned().collect::<Vec<_>>();
+    assert_eq!(
+        forward,
+        [0, 1, 7, 2],
+        "{:?}, {:?} {:?}, {:?}",
+        list.cursor,
+        head,
+        next0,
+        next1
+    );
+}
